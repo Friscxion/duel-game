fce31c23c56ed8767d8c6a0e1a085e91ab3942b9
Erwann <erwann.champion@outlook.fr>
2021-11-10 11:11:11 +0100

    Middleware Auth

--- user_service/api/knex_instance.js
+++ user_service/api/knex_instance.js
@@ -2,11 +2,11 @@ const mdb = require('knex-mariadb');
 const knex = require('knex')({
     client: mdb,
     connection: {
-        host : 'db',
-        port : 3306,
-        user : 'tdl_db',
-        password : 'password',
-        database : 'tdl_db'
+        host : process.env.HOST,
+        port : process.env.PORT,
+        user : process.env.USER,
+        password : process.env.PASSWORD,
+        database : process.env.DATABASE
     }
 });
 
