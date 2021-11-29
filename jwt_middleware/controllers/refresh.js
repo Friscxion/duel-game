019b7c81e07e5e7ca91fbd5560d4deadc914e5f3
Erwann <erwann.champion@outlook.fr>
2021-11-29 15:39:51 +0100

    Refresh token

--- jwt_middleware/controllers/refresh.js
+++ jwt_middleware/controllers/refresh.js
@@ -4,12 +4,12 @@ const axios = require("axios");
 
 const refreshToken = (req, res) => {
 
-    axios.post("http://localhost:3002/refresh_token",{
+    axios.post("http://host.docker.internal:3002/refresh_token",{
         nickname:req.user.nickname
     }).then(({data})=>{
         res.status(200).send({token:data});
     }).catch((e) => {
-        this.setState({state:"failed"})
+       console.log(e)
     });
 
 };
